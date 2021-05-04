f2b403880b76390c093b1c09f61c544630b1adc0
Nicolas Keller <nicolas.kllr@gmail.com>
2021-05-04 21:58:21 +0200

    Update headers

--- api/index.ts
+++ api/index.ts
@@ -16,21 +16,24 @@ type VercelResponse = {
 export default async (req: VercelRequest, res: VercelResponse) => {
   res.headers = {
     'Access-Control-Allow-Origin': 'https://carotte.netlify.app',
-    'Access-Control-Allow-Methods': req.headers['Access-Control-Request-Method'],
-    'Access-Control-Allow-Headers': req.headers['Access-Control-Request-Headers'],
+    'Vary': 'Origin',
     'Access-Control-Allow-Credentials': 'true',
   }
 
   if (req.method === 'OPTIONS') {
-    res.send()
+    res.headers['Access-Control-Allow-Methods'] = 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    res.headers['Access-Control-Allow-Headers'] = req.headers['Access-Control-Request-Headers']
+    res.headers['Content-Length'] = '0'
+    res.headers['Vary'] = 'Origin,Access-Control-Request-Headers'
+
+    res.status(204).send()
     return
   }
 
   const url = req.query.url
 
   if (!url) {
-    res.status(404)
-    res.send()
+    res.status(404).send()
     return
   }
 
